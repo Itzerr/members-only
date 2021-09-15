aae5f2e18f2f87d35e0a936687a3ed8c8a79207d
Itzerr <eatzer@gmail.com>
2021-09-15 23:29:16 +0200

    initial

---
+++ app/models/member.rb
@@ -0,0 +1,9 @@
+class Member < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable, authentication_keys: [:username]
+  has_many :posts
+
+  validates :username, presence: true, uniqueness: true
+end
